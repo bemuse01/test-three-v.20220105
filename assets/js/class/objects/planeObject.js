ac9ae4e81db01b10559013f09789bdfb7b4adeb2
bemuse01 <sequeland@gmail.com>
2022-01-10 09:38:03 +0900

    Update planeObject.js

--- assets/js/class/objects/planeObject.js
+++ assets/js/class/objects/planeObject.js
@@ -38,6 +38,9 @@ export default class{
         const indexArr = this.index.array
         const indexCnt = this.index.count
 
+        const w = this.widthSeg * 2
+        const h = this.heightSeg
+
         const positions = []
         const normals = []
 
@@ -48,49 +51,51 @@ export default class{
         const cb = new THREE.Vector3()
         const ab = new THREE.Vector3()
 
-        for(let i = 0; i < indexCnt / 3; i++){
-            const idx = i * 3
+        for(let i = 0; i < h; i++){
+            for(let j = 0; j < w; j++){
+                const idx = (i * w + j) * 3
 
-            const p1 = indexArr[idx] * 3
-            const p2 = indexArr[idx + 1] * 3
-            const p3 = indexArr[idx + 2] * 3
+                const p1 = indexArr[idx] * 3
+                const p2 = indexArr[idx + 1] * 3
+                const p3 = indexArr[idx + 2] * 3
 
-            const a = 0
-            const b = 0
+                const a = 0
+                const b = 0
 
-            const x1 = posArr[p1] + a
-            const y1 = posArr[p1 + 1] + b
-            const z1 = posArr[p1 + 2]
+                const x1 = posArr[p1] + a
+                const y1 = posArr[p1 + 1] + b
+                const z1 = posArr[p1 + 2]
 
-            const x2 = posArr[p2] + a
-            const y2 = posArr[p2 + 1] + b
-            const z2 = posArr[p2 + 2]
+                const x2 = posArr[p2] + a
+                const y2 = posArr[p2 + 1] + b
+                const z2 = posArr[p2 + 2]
 
-            const x3 = posArr[p3] + a
-            const y3 = posArr[p3 + 1] + b
-            const z3 = posArr[p3 + 2]
+                const x3 = posArr[p3] + a
+                const y3 = posArr[p3 + 1] + b
+                const z3 = posArr[p3 + 2]
 
-            positions.push(x1, y1, z1)
-            positions.push(x2, y2, z2)
-            positions.push(x3, y3, z3)
+                positions.push(x1, y1, z1)
+                positions.push(x2, y2, z2)
+                positions.push(x3, y3, z3)
 
-            pA.set(x1, y1, z1)
-            pB.set(x2, y2, z2)
-            pC.set(x3, y3, z3)
+                pA.set(x1, y1, z1)
+                pB.set(x2, y2, z2)
+                pC.set(x3, y3, z3)
 
-            cb.subVectors(pC, pB)
-            ab.subVectors(pA, pB)
-            cb.cross(ab)
+                cb.subVectors(pC, pB)
+                ab.subVectors(pA, pB)
+                cb.cross(ab)
 
-            cb.normalize()
+                cb.normalize()
 
-            const nx = cb.x
-            const ny = cb.y
-            const nz = cb.z
+                const nx = cb.x
+                const ny = cb.y
+                const nz = cb.z
 
-            normals.push(nx, ny, nz)
-            normals.push(nx, ny, nz)
-            normals.push(nx, ny, nz)
+                normals.push(nx, ny, nz)
+                normals.push(nx, ny, nz)
+                normals.push(nx, ny, nz)
+            }
         }
 
         geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
@@ -115,6 +120,11 @@ export default class{
 
     // resize
     resize({width, height, widthSeg, heightSeg}){
+        this.width = width
+        this.height = height
+        this.widthSeg = widthSeg
+        this.heightSeg = heightSeg
+        
         const plane = new THREE.PlaneGeometry(width, height, widthSeg, heightSeg)
 
         this.position = plane.attributes.position
